9be7f1ab4040b228b121ddac28fb4ee57cb63405
Saša Barišić <cartman300@net.hr>
2015-08-31 21:37:16 +0200

    Project files

---
+++ Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lua.NET;
+
+namespace Test {
+	class Program {
+		static string ReadLine(string Prompt) {
+			Console.Write(Prompt);
+			return Console.ReadLine();
+		}
+
+		static void Main(string[] args) {
+			Console.Title = "Test";
+
+			string Str;
+			while ((Str = ReadLine("> ")).Length > 0)
+				try {
+
+				} catch (Exception E) {
+					Console.WriteLine(E.Message);
+				}
+
+			Console.WriteLine("Done!");
+			Console.ReadLine();
+		}
+	}
+}
